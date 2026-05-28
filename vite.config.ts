0db76cdf23ccccb896ac60e10b6c05a74d9886c0
Beatrice <salumeriaenotecagigliola@gmail.com>
2026-05-28 18:56:35 +0200

    v.20

--- vite.config.ts
+++ vite.config.ts
@@ -30,9 +30,15 @@ export default defineConfig(({mode}) => {
           icons: [
             {
               src: 'icon.png',
-              sizes: '512x512',
+              sizes: '192x192 512x512',
               type: 'image/png',
-              purpose: 'any maskable'
+              purpose: 'any'
+            },
+            {
+              src: 'icon.png',
+              sizes: '192x192 512x512',
+              type: 'image/png',
+              purpose: 'maskable'
             }
           ]
         }
